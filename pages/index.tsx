ed40bff3f77e890e12ac596e56f6c33f71db46c1
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-08-21 03:10:30 +0200

    built: setup styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+
+const StyledIndex = styled.h1`
+  color: red;
+  font-size: 2rem;
+  padding: 2em;
+`;
+
 const Index = () => {
-  return <div>Index</div>;
+  return <StyledIndex>Index</StyledIndex>;
 };
 
 export default Index;
