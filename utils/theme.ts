6061d086ff448f8c7fceee23806a68a3440b501f
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-08-21 05:25:15 +0200

    feat: add root css settings

---
+++ utils/theme.ts
@@ -0,0 +1,49 @@
+const padding_zero = `0em`;
+const padding_small = `0.25em`;
+const padding_medium = `0.5em`;
+const padding_large = `1em`;
+const padding_xlarge = `2em`;
+
+const margin_zero = `0em`;
+const margin_small = `0.25em`;
+const margin_medium = `0.5em`;
+const margin_large = `1em`;
+const margin_xlarge = `2em`;
+
+const font_size_small = `0.75em`;
+const font_size_medium = `1em`;
+const font_size_large = `1.5em`;
+const font_size_xlarge = `2em`;
+
+const heart_color = `#e25555`;
+const border_color = `#e6e6e6`;
+const header_color = `#fff`;
+const header_border_color = header_color;
+const border_width = `2px`;
+
+/* 
+String can't have dash
+Object can't have underscore
+*/
+
+export const theme = {
+  'padding-zero': padding_zero,
+  'padding-small': padding_small,
+  'padding-medium': padding_medium,
+  'padding-large': padding_large,
+  'padding-xlarge': padding_xlarge,
+  'margin-zero': margin_zero,
+  'margin-small': margin_small,
+  'margin-medium': margin_medium,
+  'margin-large': margin_large,
+  'margin-xlarge': margin_xlarge,
+  'font-size-small': font_size_small,
+  'font-size-medium': font_size_medium,
+  'font-size-large': font_size_large,
+  'font-size-xlarge': font_size_xlarge,
+  'heart-color': heart_color,
+  'border-color': border_color,
+  'header-color': header_color,
+  'header-border-color': header_border_color,
+  'border-width': border_width,
+};
